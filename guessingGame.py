550b40b982c888ddc5d37e1145cb8602e9ae0a9e
‪Rafat Ahmad‬‏ <37984571+RafatBAhmad@users.noreply.github.com>
2021-08-13 23:13:01 +0300

    Edit the code to improve it

--- guessingGame.py
+++ guessingGame.py
@@ -11,40 +11,52 @@ import sys
 print("---------------------------------------------")
 print("Hello and welcome to the Number Guessing Game")
 print("---------------------------------------------\n")
-HIGHSCORE = 0
+HIGHSCORE = 10
+
+
 def start_game():
-    print("\N{TACO}")
-    words = "hi rafat ahmad"
-    listi = words.split()
-    print(listi)
-    print("+".join(listi))
     randomNumber = random.randint(1,10)
-    try:
+    while True:
+      try:
         yourGuess = int (input("Enter your guess number between 1 - 10: "))
-    except  ValueError:
+        if type(yourGuess) is int:
+          break
+      except  ValueError:
         print("Please enter intger number.")   
-        yourGuess = int (input("Enter your guess number between 1 - 10: "))
 
     count  = 1
     while yourGuess != randomNumber:
-      if yourGuess >  randomNumber:
-        print("\n It's lower")
-      elif yourGuess <  randomNumber:
-        print("\n It's higher")
-      yourGuess = int (input("Enter your guess number between 1 - 10: "))
+      if 0 < yourGuess < 11:
+        if yourGuess >  randomNumber:
+          print("\n It's lower")
+        elif yourGuess <  randomNumber:
+          print("\n It's higher")
+      else:
+        print("plase make sure your guess must be between 1 - 10")    
+
+      while True:
+        try:
+          yourGuess = int (input("Enter your guess number between 1 - 10: "))
+          if type(yourGuess) is int:
+            break
+        except  ValueError:
+          print("Please enter intger number.")   
       count += 1
+
+
     print("\n You got it, It took you {} tries.\n".format(count))
+    WYLTPA = input("Would you like to play again? [y]es/[n]o: ") # WYLTPA == Would You Like To Play Again
+    global HIGHSCORE
+    if HIGHSCORE > count:
+      HIGHSCORE = count
     
-    WYLTPA = input("Would you like to play again? [y]es/[n]o: ")
-    HIGHSCORE = count
+    
     if WYLTPA.lower() == "y":
       print("The HIGHSCORE is {}".format(HIGHSCORE))
       start_game()
       
-    sys.exit("\n Closing game, see you next time!")  
+    sys.exit("\n Closing game,your HIGHSCORE for this round is {}, see you next time!".format(HIGHSCORE))  
         
 
-#clear && python guessing_game.py
-# Kick off the program by calling the start_game function.
 start_game()
   
